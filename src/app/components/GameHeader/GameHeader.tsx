d579877d08b00ef323c90a1a675941b462ca5b93
one-thunder <one@thunderstorm.pro>
2021-12-27 17:32:36 +0200

    Restructure GameHeader component

--- src/app/components/GameHeader/GameHeader.tsx
+++ src/app/components/GameHeader/GameHeader.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { S } from './GameHeader.styled';
-import { Logo, ELogoVariant } from './Logo/Logo';
-import { Score } from './Score/Score';
+import { Logo, ELogoVariant } from './components/Logo/Logo';
+import { Score } from './components/Score/Score';
 
 export const GameHeader = (): JSX.Element => (
    <S.GameHeader variant="outlined" elevation={0} data-testid="GameHeader">

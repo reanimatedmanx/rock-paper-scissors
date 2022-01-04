4f6620c231abeb2f031b4ad9ed35b7254689953e
one-thunder <one@thunderstorm.pro>
2022-01-04 02:22:07 +0200

    Fixed alignment for Versus component

--- src/app/components/Layout/Playground/Playground.styled.tsx
+++ src/app/components/Layout/Playground/Playground.styled.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Playground = styled.main`
    display: flex;
+   justify-content: center;
    height: 100%;
    min-height: 450px;
 `;
--- src/app/components/Versus/Versus.styled.tsx
+++ src/app/components/Versus/Versus.styled.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const Versus = styled.div`
+   display: flex;
+   justify-content: center;
    min-height: 85px;
 `;
 
